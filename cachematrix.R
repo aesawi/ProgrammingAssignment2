867b913bb11ddfce075fc4e41f54f361af402eea
aesawi <issaoui2020@yahoo.fr>
2014-09-20 10:42:46 -0700

    use cache in inversion matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,27 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat_inv <- NULL
+  set <- function(y) {
+    x <<- y
+    mat_inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) mat_inv <<- inverse
+  getinverse <- function() mat_inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mat_inv <- x$getinverse()
+  if(!is.null(mat_inv)) {
+    message("getting cached data.")
+    return(mat_inv)
+  }
+  data <- x$get()
+  mat_inv <- solve(data)
+  x$setinverse(mat_inv)
+  mat_inv
 }
